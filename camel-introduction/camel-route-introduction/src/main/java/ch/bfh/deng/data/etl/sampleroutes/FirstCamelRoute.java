20f2681dc08db9781ca6293ac970ef96a66fa33b
Erik Graf <e.graf@cortical.io>
2024-03-13 15:55:27 +0100

    Update FirstCamelRoute.java to new Camel Run Style

--- camel-introduction/camel-route-introduction/src/main/java/ch/bfh/deng/data/etl/sampleroutes/FirstCamelRoute.java
+++ camel-introduction/camel-route-introduction/src/main/java/ch/bfh/deng/data/etl/sampleroutes/FirstCamelRoute.java
@@ -1,24 +1,14 @@
-package ch.bfh.deng.data.etl.sampleroutes;
+// camel-k: language=java
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.impl.DefaultCamelContext;
 
-public class FirstCamelRoute {
+public class FileRoute extends RouteBuilder {
 
-    public static void main(String args[]) throws Exception {
-        CamelContext context = new DefaultCamelContext();
-        context.addRoutes(new RouteBuilder() {
-            public void configure() {
-                from("file:/tmp/inbox")
+    @Override
+    public void configure() throws Exception {
+
+        // Write your routes here, for example:
+        from("file:/tmp/inbox")
                         .to("file:/tmp/delivery");
-
-            }
-        });
-        context.start();
-        Thread.sleep(500000);
-        context.stop();
     }
 }
-
-
